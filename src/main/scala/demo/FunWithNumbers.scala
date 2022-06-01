785352d5c2bbe9fd303c69391d2578d3493879e6
Imtiaz Ahmed <imtiaz@visena.com>
2022-06-01 11:16:54 +0600

    fun with numbers updated

---
+++ src/main/scala/demo/FunWithNumbers.scala
@@ -0,0 +1,32 @@
+package demo
+
+object FunWithNumbers extends App{
+  class Number(var value: Int){
+    def + (other: Number): Int = { value += other.value; value}
+  }
+
+  object Number{
+    private var cache = Map.empty[Int, Number]
+
+    def make(int: Int): Number =
+      cache.getOrElse(
+        int, {
+          val number = new Number(int)
+          cache += (int -> number)
+          number
+        }
+      )
+  }
+
+  implicit def int2Number(i:Int): Number = Number.make(i)
+
+  def println(n: Number): Unit = scala.Predef.println(n.value)
+
+  // POP QUIZ!
+  println(10 + 15)
+  println(5 + 30)
+  println(10 + 5)
+
+  println(5)
+  println(10)
+}
